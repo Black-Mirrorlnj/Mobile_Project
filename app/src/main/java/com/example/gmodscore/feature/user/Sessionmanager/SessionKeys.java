c9a8d739dad8fc107f734bbc4a87a737788dfef1
noronham464-design <noronham464@gmail.com>
2026-05-14 09:31:36 -0400

    Update and rename oi to SessionKeys.java

---
+++ app/src/main/java/com/example/gmodscore/feature/user/Sessionmanager/SessionKeys.java
@@ -0,0 +1,12 @@
+package com.example.gmodscore.util.session;
+
+public final class SessionKeys {
+    private SessionKeys() {}
+
+    public static final String PREF_NAME  = "gmod_session";
+    public static final String KEY_ID      = "usuario_id";
+    public static final String KEY_NOME    = "usuario_nome";
+    public static final String KEY_LOGIN   = "usuario_login";
+    public static final String KEY_PERFIL  = "usuario_perfil";
+    public static final String KEY_LOGADO  = "logado";
+}
